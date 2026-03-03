73aae9f7b5fe8a3a3901e7cee54daabdc0410fed
awez78 <shaikhawez7178@gmail.com>
2026-03-03 11:45:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder: Shaikh Awez
+# Date:10-02-2026
 
 print("--- Extracting Words from Text File ---\n")
+num = int(input(""))
+with open ("story.txt", 'r') as f:
+    content = f.read().split()
+    wordList = [ i.lower() for i in content if(num == len(i))]
+    wordList2 = sorted(set(wordList))
+    print(f"{wordList2}")
+
 
